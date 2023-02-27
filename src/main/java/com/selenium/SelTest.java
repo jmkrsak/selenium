103b8ac1a4cde47fda343007a9c13379274ed6bd
John-Michael Krsak <jmkrsak@gmail.com>
2023-02-27 14:09:57 -0500

    set key and value for chromedriver

--- src/main/java/com/selenium/SelTest.java
+++ src/main/java/com/selenium/SelTest.java
@@ -1,9 +1,30 @@
 package com.selenium;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+//Class for testing selenium
 public class SelTest {
 
-    // class for testing selenium
+    public static void main(String[] args) {
+
+        //Invoke Browser
+
+        //Chrome - ChromeDriver -> methods
+        //methods have the same name for all browsers
+        //ChromeDriver driver = new ChromeDriver();
+
+        //chromedriver -> invokes Chrome browser
+        //webdriver.chrome.driver -> value of path
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+
+        //WebDriver - control interface for all browsers -> methods
+        //WebDriver methods + Class methods
+        WebDriver driver = new ChromeDriver();
 
 
+
+
+    }
 
 }
